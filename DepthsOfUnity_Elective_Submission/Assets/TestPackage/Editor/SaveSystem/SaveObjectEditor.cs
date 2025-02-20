c82da6d320d947fda2fc77fdd9abf5595ad8eae5
Lukas <lukas.salewsky@gmail.com>
2025-02-20 22:13:36 +0100

    SaveSystem Buttons revived + cleanup

--- DepthsOfUnity_Elective_Submission/Assets/TestPackage/Editor/SaveSystem/SaveObjectEditor.cs
+++ DepthsOfUnity_Elective_Submission/Assets/TestPackage/Editor/SaveSystem/SaveObjectEditor.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEditor;
-using System;
+using SimpleSaveSystem;
 
 [CustomEditor(typeof(SaveObject))]
 public class SaveObjectEditor : Editor
@@ -9,18 +9,38 @@ public class SaveObjectEditor : Editor
     {
         base.OnInspectorGUI();
 
-        SaveObject targetScript = (SaveObject)target;
+        DrawButton(target as UnityEngine.Object);
 
+        SaveObject targetScript = (SaveObject)target;
         if (targetScript.SaveFileName == "")
         {
             EditorGUILayout.HelpBox("Save File Name missing", MessageType.Warning);
         }
-
-        SaveObjectData saveObjectData = targetScript.saveObjectData;
-
         if (!targetScript.IsEveryDataNamed())
         {
             EditorGUILayout.HelpBox("At lease one Value Name missing", MessageType.Warning);
+        }
+    }
+
+    private void DrawButton(UnityEngine.Object target)
+    {
+        var methodInfos = target.GetType().GetMethods(System.Reflection.BindingFlags.Instance
+            | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic);
+
+        foreach (var methodInfo in methodInfos)
+        {
+            var attributes = methodInfo.GetCustomAttributes(typeof(ButtonAttribute), true);
+            if (attributes.Length > 0)
+            {
+                var buttonAttribute = attributes[0] as ButtonAttribute;
+                string label = string.IsNullOrEmpty(buttonAttribute.Label) ?
+                    methodInfo.Name : buttonAttribute.Label;
+
+                if (GUILayout.Button(label))
+                {
+                    methodInfo.Invoke(target, null);
+                }
+            }
         }
     }
 }
