7476384db1c38db8079c0bf176fd0dca089f032c
Lukas <lukas.salewsky@gmail.com>
2025-02-21 19:24:07 +0100

    Rearrange & Adding ExtentionMethods

---
+++ DepthsOfUnity_Elective_Submission/Assets/SubmissionPackage/Runtime/Scripts/InterpolateExtensionMethods/InterpolateEase.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public enum EaseType
+{
+    Linear,
+    InSine,
+    OutSine,
+    InOutSine,
+}
+
+[CreateAssetMenu(fileName = "InterpolateEase", menuName = "InterpolateExtensionMethods/InterpolateEase")]
+public class InterpolateEase : ScriptableObject
+{
+    static InterpolateEase instance;
+
+    public static InterpolateEase Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<InterpolateEase>("Data/InterpolateExtensionMethods/InterpolateEaseData");
+            }
+            return instance;
+        }
+    }
+
+    public AnimationCurve GetEase(EaseType easeType)
+    {
+        switch (easeType)
+        {
+            case EaseType.InSine:
+                return inSine;
+            case EaseType.OutSine:
+                return outSine;
+            case EaseType.Linear:
+                return linear;
+            case EaseType.InOutSine:
+                return inOutSine;
+            default:
+                return linear;
+        }
+    }
+
+    [SerializeField] private AnimationCurve linear;
+    [SerializeField] private AnimationCurve inSine;
+    [SerializeField] private AnimationCurve outSine;
+    [SerializeField] private AnimationCurve inOutSine;
+}
